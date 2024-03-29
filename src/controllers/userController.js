76eabed906a8dc9d38a9f3ac5a0aaed161dc14ab
Vinod <sagar.choudhary9811@gmail.com>
2024-03-29 12:07:12 -0500

    Nitin

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,7 @@ export const addUsers = async (req, res, next) => {
         await User.create({
             name: 'Nihar'
         }).then(res => { "then chali" }).catch(err => { "Err" })
-        res.status(201).json({ msg: "New Applicartion changes" })
+        res.status(201).json({ msg: "New Applicartion changes  Nitin" })
     } catch (error) {
         console.log(error);
         res.status(500).json({ msg: "Error" })
