8aaab5c39a6c02ca89d8deb93e343551bdf4ed90
Vinod <sagar.choudhary9811@gmail.com>
2024-03-31 13:03:33 -0500

    install some dep

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,12 +1,36 @@
 
 import mongoose from "mongoose";
-
+const genderEnum = ['male', 'female', 'other'];
 const userSchema = new mongoose.Schema({
+
     name: {
         type: String,
+        trim: true
     },
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+    }, password: {
+        type: String
+    },
+    contact: {
+        type: Number,
+        trim: true
+    },
+    profile: {
+        type: String,
+        trim: true
+    },
+    age: {
+        type: Number
+    },
+    gender: {
+        type: String,
+        enum: genderEnum
+    }
 });
 
-const User = mongoose.model('User', userSchema); // Changed 'vandor' to 'Vendor'
+const User = mongoose.model('User', userSchema);
 
 export default User;
